0480e79800091f6e8e38f1773c35b6c078f6cda8
Edson Luiz Zeballos dos Santos Filho <edson.filho@ms.senac.br>
2021-06-07 01:28:21 -0400

    exemplo com data annotation

--- EFCore/Domain/Cliente.cs
+++ EFCore/Domain/Cliente.cs
@@ -1,10 +1,16 @@
-using System.Runtime.ConstrainedExecution;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EFCore.Domain
 {
+    [Table("tbl_clientes")]
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Column("primeiro_telefone")]
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
